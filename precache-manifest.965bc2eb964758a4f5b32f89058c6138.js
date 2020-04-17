82bfe28b2ed589f23535270b97ca2a5f76d24705
taylanhakan <hakan.taylan@aurea.com>
2020-04-17 15:43:42 +0300

    Updates

---
+++ precache-manifest.965bc2eb964758a4f5b32f89058c6138.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7168c9b761ec7ee3422fba4d223836a",
+    "url": "/deneme/index.html"
+  },
+  {
+    "revision": "9afc9522b4867f91d669",
+    "url": "/deneme/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0bb857a6cdf8e8eae7ea",
+    "url": "/deneme/static/js/2.eb7dced0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deneme/static/js/2.eb7dced0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9afc9522b4867f91d669",
+    "url": "/deneme/static/js/main.26f64fad.chunk.js"
+  },
+  {
+    "revision": "b939612c2dfd209fbefc",
+    "url": "/deneme/static/js/runtime-main.8609b107.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deneme/static/media/logo.5d5d9eef.svg"
+  }
+]);
